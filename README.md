801d29483ba5fb5eb9122612d8c3053f1da021ca
Emersson <ortizjacobo25@gmail.com>
2023-03-20 17:41:03 -0500

    MI PORTAFOLIO

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portafolio
+
+## Acerca del progromador
+Autor: Emersson Ortiz Jacobo
+Mi carrera es: **Ingeneria de Software con IA**
+Puedes escribir a mi correo: ortizjacobo25@gmail.com
+
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function saludar(){
+  alert("Bievenido a mi Portafolio");
+  
+ }
+ function presentarse(){
+   alert("Te saluda Emersson, mucho gusto")
+ }
+ const botonSaludo = document.getElementById("saludo");
+ botonSaludo.addEventListener("click", saludar);
+ 
